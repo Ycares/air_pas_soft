3762212e8dd1062bd2544c9fc2efbba851a05cc2
Almis Lipon <almislpn@gmail.com>
2023-05-30 16:36:03 +0200

    delete routes inutile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'weapons/index'
-  get 'weapons/show'
-  get 'weapons/edit'
-  get 'weapons/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
