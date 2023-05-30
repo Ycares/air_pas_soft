efa0e7b0687d3f003896bf1ac0e96d053f77d808
vincent helpin <vh_abo@protonmail.com>
2023-05-31 01:19:50 +0200

    small change in order model - seeds work fine, todo : check if weapons have pictures...

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,9 @@ class Order < ApplicationRecord
   belongs_to :weapon
   belongs_to :user
 
-  validates :comment, length: { minimum: 6 }
+  # TO DO : problem with validates for min length : how to deal with order to come ?(no comment ! )
+  # validates :comment, length: { minimum: 6 }
 
+  # TO DO : add a min value for total_price : must be > unit price !
+  # validates :comment, length: { minimum: 6 }
 end
