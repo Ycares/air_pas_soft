6b6a6dfffde01719b390275d416d72ce1800d04a
Almis Lipon <almislpn@gmail.com>
2023-05-31 11:22:28 +0200

    Devise parametrer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # to do : ajout de champ pour formulaire inscription
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   # For additional fields in app/views/devise/registrations/new.html.erb
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :experience])
 
-  #   # For additional in app/views/devise/registrations/edit.html.erb
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  # end
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :age, :experience])
+  end
 end
