e5035dc2fa9eb43fa319583d2dbcc3a66b8e761d
Almis Lipon <almislpn@gmail.com>
2023-06-01 17:58:14 +0200

    Add a weapon form en cours

--- app/controllers/owner/weapons_controller.rb
+++ app/controllers/owner/weapons_controller.rb
@@ -1,7 +1,13 @@
 class Owner::WeaponsController < ApplicationController
-
   def index
     @user = current_user
     @weapons = current_user.weapons
+  end
+
+  def new
+    @weapon = Weapon.new
+  end
+
+  def create
   end
 end
