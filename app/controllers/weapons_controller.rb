0cc45a8ab55b6dc87a99fc183bdaee9749f9299b
Almis Lipon <almislpn@gmail.com>
2023-06-02 14:29:07 +0200

    search bar done

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -2,6 +2,10 @@ class WeaponsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @weapons = Weapon.all
+    if params[:query].present?
+      sql_subquery = "name ILIKE :query OR origin_country ILIKE :query"
+      @weapons = @weapons.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def show
